97aa1082f3c4e3ef4ec76dd3c4937b6ace323547
rostunic <=>
2024-04-06 12:02:16 +0200

    Alias can be selected

--- main.ts
+++ main.ts
@@ -1,134 +1,79 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor,FuzzySuggestModal, LinkCache, MarkdownView, Modal, Plugin, TFile, parseLinktext } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+class AliasPicker extends FuzzySuggestModal<string> {
+	getItems(): string[] {
+		return this.aliases;
+	}
+	getItemText(item: string): string {
+		return item;
+	}
+	onChooseItem(item: string, evt: MouseEvent | KeyboardEvent): void {
+		this.chooseItem(item);
+	}
+	constructor(app: App, private targetFile: TFile, private link: LinkCache, private aliases: string[],
+		private editor: Editor
+	) {
+		super(app);
+
+	}
+	chooseItem(item: string) {
+		const start = this.editor.offsetToPos(this.link.position.start.offset);
+		const end = this.editor.offsetToPos(this.link.position.end.offset);
+		const parsed = parseLinktext(this.link.original);
+		const newLink = this.app.fileManager.generateMarkdownLink(this.targetFile, parsed.path, parsed.subpath, item);
+		this.editor.replaceRange(newLink, start, end);
+		const newPosition = this.editor.offsetToPos(this.link.position.start.offset + newLink.length + 1);
+		this.editor.setCursor(newPosition);
+	}
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: 'opsidian-alias-picker-pick-alias',
+			name: 'Pick alias',
 			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+				const activeEditor = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (!activeEditor) return;
+				const editor = activeEditor?.editor;
+				const currentFile = activeEditor.file;
+				if (!currentFile || !editor) return;
+				const currentCache = this.app.metadataCache.getFileCache(currentFile);
+				const links = currentCache?.links;
+				if (!links) return;
+				const cursorOffset = editor.posToOffset(editor.getCursor());
+				const currentLink = links.find(x => x.position.start.offset <= cursorOffset && x.position.end.offset >= cursorOffset)
+				if (!currentLink) return;
+				const file = this.app.vault.getFileByPath(currentLink.link);
+				if (!file) return;
+				const fileCache = this.app.metadataCache.getFileCache(file);
+				if (!fileCache?.frontmatter) return;
+				const aliases: string[] | undefined = fileCache.frontmatter.aliases;
+				if (!aliases) return;
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				const allowedNames = [...aliases, file.basename];
+				if (currentLink.displayText)
+					allowedNames.remove(currentLink.displayText)
+				if (allowedNames.length === 0) return;
+
+				if (!checking) {
+					const aliasPicker = new AliasPicker(this.app, file, currentLink, allowedNames, editor);
+					if (allowedNames.length === 1) {
+						aliasPicker.chooseItem(allowedNames[0]);
+						return;
+					}
+					aliasPicker.open();
 				}
+
+				return true;
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
